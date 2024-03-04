8b4e07026721d424a196b451756e73f6c1280747
Betzy V. Rosiles Moreno <brosilesmoreno@bsd220.org>
2024-03-04 09:50:37 -0600

    Initial Commit

---
+++ API list/API_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_listApp.swift
+//  API list
+//
+//  Created by Betzy Moreno on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct API_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
