d3f862133f37f4c2356f7f35feeb2e0486b4dee4
Betzy V. Rosiles Moreno <brosilesmoreno@bsd220.org>
2024-03-05 09:53:07 -0600

    "Finished setting up alert"

--- API list/ContentView.swift
+++ API list/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var categories = [String]()
+    @State private var showingAlert = false
     var body: some View {
         NavigationView {
             List(categories, id: \.self) { category in
@@ -20,6 +21,11 @@ struct ContentView: View {
         }
         .task {
             await getCategories()
+                .alert(isPresented: $showingAlert) {
+                    Alert(title: Text("Loading Error"),
+                          message: Text("There was a problem loading the API catagories"),
+                          dismissButton: .default(Text("OK")))
+                }
         }
     }
     
@@ -30,9 +36,11 @@ struct ContentView: View {
                 if let decodedResponse = try?
                     JSONDecoder().decode(Categories.self, from: data) {
                     categories = decodedResponse.categories
+                    return
                 }
             }
         }
+        showingAlert = true
     }
 }
 struct ContentView_Previews: PreviewProvider {
